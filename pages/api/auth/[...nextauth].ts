b54a802baafdd11c31d7dfc78f58940f71f2e055
thasuohm <thasuohm@gmail.com>
2023-01-14 20:04:14 +0700

    update env val

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,14 +1,11 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
-const clientId = process.env.GOOGLE_CLIENT_ID ?? ''
-const clientSecret = process.env.GOOGLE_SECRET ?? ''
-
 export default NextAuth({
   providers: [
     GoogleProvider({
-      clientId,
-      clientSecret,
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
     }),
   ],
 })
